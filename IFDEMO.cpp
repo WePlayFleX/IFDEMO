7b748f7ef6b9367249cbf9697398479feee97fc4
WePlayFleX <alex.kamai@ya.ru>
2021-12-10 22:27:26 +0300

    Добавьте файлы проекта.

---
+++ IFDEMO.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x;
+	cout << "Enter number: ";
+	cin >> x;
+	if (x > 100)
+	{
+		cout << "This number is greater than 100\n ";
+	}
+
+	system("pause");
+	return 0;
+}
